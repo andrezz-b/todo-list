af1e0c79cf54b98b5be5378e04b8fdcc8244bb17
Andrej Bozic <abozic389@gmail.com>
2022-02-08 15:47:10 +0100

    Dynamically creating todo items

--- src/scripts/createElement.js
+++ src/scripts/createElement.js
@@ -1,6 +1,36 @@
-export function createExpanded() {
+export const createTodoElement = (data) => {
+	const todoEl = createDiv("todo-item");
+
+	const container = createDiv("container");
+	const priority = createDiv("priority", data.priority);
+
+	const label = document.createElement("label");
+	const checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	label.append(checkbox);
+
+	const title = document.createElement("h3");
+	title.classList.add("todo-title");
+	title.textContent = data.title;
+
+	const dueDate = createDiv("due-date");
+	dueDate.textContent = data.date;
+
+	const trashIcon = document.createElement("i");
+	trashIcon.setAttribute("class", "far fa-trash-alt");
+
+	container.append(priority, label, title, dueDate, trashIcon);
+	const expanded = createExpanded(data.desc);
+
+	todoEl.append(container, expanded);
+	todoEl.setAttribute("data-expand", "false");
+
+	return todoEl;
+};
+
+const createExpanded = (text) => {
 	const divExpanded = document.createElement("div");
-	divExpanded.classList.add("expanded");
+	divExpanded.classList.add("hidden");
 
 	const desc = document.createElement("div");
 	const subtitle = document.createElement("h1");
@@ -9,11 +39,19 @@ export function createExpanded() {
 
 	const par = document.createElement("p");
 	par.classList.add("par--desc");
-	par.textContent =
-		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Id distinctio, velit eaque voluptates ea adipisci itaque repellat vitae minima numquam.";
+	par.textContent = text;
 
 	desc.append(subtitle, par);
 	divExpanded.append(desc);
 
 	return divExpanded;
-}
+};
+
+const createDiv = (...className) => {
+	const div = document.createElement("div");
+	className.forEach((el) => {
+		div.classList.add(el);
+	});
+
+	return div;
+};
